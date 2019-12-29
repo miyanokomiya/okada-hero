79bfec5bee85af0cd1f1204f2fdb5947e32fbc3e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-12-29 10:24:57 +0900

    fix

--- src/App.ts
+++ src/App.ts
@@ -1,42 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three-orbitcontrols-ts'
-import { Font, load } from 'opentype.js'
-import okageo, { IVec2, ISvgPath, ISvgStyle } from 'okageo'
-
-let _font: Font | null
-const fontURL = 'https://fonts.gstatic.com/ea/notosansjapanese/v6/NotoSansJP-Medium.otf'
-
-export function loadFont(): Promise<Font> {
-  return new Promise((resolve, reject) => {
-    if (_font) resolve(_font)
-
-    load(fontURL, (err, font) => {
-      if (!font || err) {
-        return reject(new Error('Font could not be loaded: ' + err))
-      }
-      return resolve(font)
-    })
-  })
-}
-
-export async function parseFont(text: string, style: ISvgStyle): Promise<ISvgPath[]> {
-  const font = await loadFont()
-  const lines = text.split(/\n|\r\n/)
-  const size = 72
-  let pathList: IVec2[][] = []
-  lines.forEach((line, i) => {
-    pathList = pathList.concat(
-      okageo.svg
-        .parseOpenPath(font.getPath(line, 0, size * 1.1 * i, size))
-        .map(info => okageo.geo.omitSamePoint(info.d)),
-    )
-  })
-
-  return okageo.geo.getIncludedPolygonGroups(pathList).map(group => {
-    const [d, ...included] = group
-    return { d, included, style }
-  })
-}
+import okageo, { ISvgPath, ISvgStyle } from 'okageo'
+import { parseFont, splirtGrid, IBlock, craeteBlock } from './utils'
 
 export default class App {
   width: number
@@ -48,9 +13,9 @@ export default class App {
   renderer: THREE.WebGLRenderer
   camera: THREE.PerspectiveCamera
   controls: OrbitControls
-  material: THREE.MeshBasicMaterial
+  faceMaterial: THREE.MeshBasicMaterial
   wallMaterial: THREE.MeshBasicMaterial
-  meshes: THREE.Mesh[] = []
+  blocks: IBlock[] = []
 
   constructor(args: { canvas: HTMLCanvasElement; width: number; height: number }) {
     const { width, height, canvas } = args
@@ -67,7 +32,7 @@ export default class App {
     this.camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000)
     this.camera.position.set(16, -1, 30)
     this.camera.lookAt(0, 0, 0)
-    this.material = new THREE.MeshBasicMaterial({
+    this.faceMaterial = new THREE.MeshBasicMaterial({
       color: this.style.fillStyle,
       side: THREE.DoubleSide,
     })
@@ -90,132 +55,33 @@ export default class App {
   animate = () => {
     if (!this.running) return
 
+    this.blocks.forEach(b => {
+      const axis = new THREE.Vector3(1, 0, 0).normalize()
+      const rad = Math.PI / 180 / 2
+      b.face.rotateOnWorldAxis(axis, rad)
+      b.wall.rotateOnWorldAxis(axis, rad)
+    })
     this.controls.update()
     this.renderer.render(this.scene, this.camera)
     requestAnimationFrame(this.animate)
   }
 
   createMesh(pathInfo: ISvgPath) {
-    const depth = 2
-    const wallGeometry = new THREE.Geometry()
-    const shape = new THREE.Shape()
-    pathInfo.d.forEach((v, i) => {
-      const from = { x: v.x, y: -v.y }
-      const to = {
-        x: pathInfo.d[(i + 1) % pathInfo.d.length].x,
-        y: -pathInfo.d[(i + 1) % pathInfo.d.length].y,
-      }
-
-      if (i === 0) shape.moveTo(from.x, from.y)
-      else shape.lineTo(from.x, from.y)
-
-      // はみ出ないよう側面はdepthより短くする
-      const mesh = new THREE.Mesh(
-        new THREE.PlaneGeometry(okageo.geo.getDistance(from, to), depth * 0.95),
-      )
-      const center = okageo.geo.getCenter(from, to)
-      mesh.translateX(center.x)
-      mesh.translateY(center.y)
-      mesh.translateZ(-depth / 2)
-      mesh.rotateX(-Math.PI / 2)
-      mesh.rotateY(-okageo.geo.getRadian(to, from))
-      wallGeometry.mergeMesh(mesh)
-    })
-
-    const wallMeash = new THREE.Mesh(wallGeometry, this.wallMaterial)
-    this.scene.add(wallMeash)
-    this.meshes.push(wallMeash)
-
-    const shapeMeshTop = new THREE.Mesh(new THREE.ShapeGeometry(shape), this.material)
-    const shapeMeshBottom = shapeMeshTop.clone()
-    shapeMeshBottom.translateZ(-depth)
-    const shapeGeometry = new THREE.Geometry()
-    shapeGeometry.mergeMesh(shapeMeshTop)
-    shapeGeometry.mergeMesh(shapeMeshBottom)
-    const shapeMesh = new THREE.Mesh(shapeGeometry, this.material)
-    this.scene.add(shapeMesh)
-    this.meshes.push(shapeMesh)
-  }
-
-  splitShape(shape: ISvgPath, line: IVec2[]): ISvgPath[] {
-    // 包含ポリゴンと共に全て分割
-    let splited = okageo.geo.splitPolyByLine(shape.d, line)
-    if (splited.length < 2) return [shape]
-
-    // 本体と回転方向が一致しているかで分類
-    const rootLoopwise = okageo.geo.getLoopwise(shape.d)
-    const sameLoopwiseList: IVec2[][] = []
-    const oppositeLoopwiseList: IVec2[][] = []
-    shape.included!.forEach(s => {
-      if (okageo.geo.getLoopwise(s) === rootLoopwise) {
-        sameLoopwiseList.push(s)
-      } else {
-        oppositeLoopwiseList.push(s)
-      }
-    })
-
-    // 本体と同回転のものはそのまま分割
-    sameLoopwiseList.forEach(poly => {
-      const sp = okageo.geo.splitPolyByLine(poly, line)
-      splited = [...splited, ...(sp.length > 0 ? sp : [poly])]
-    })
-
-    // 本体と逆回転のものは特殊処理
-    const notPolyList: IVec2[][] = []
-    oppositeLoopwiseList.forEach(poly => {
-      const sp = okageo.geo.splitPolyByLine(poly, line)
-      if (sp.length > 0) {
-        // 分割されたらブーリアン差をとるために集める
-        notPolyList.push(poly)
-      } else {
-        // 分割なしならそのまま
-        splited.push(poly)
-      }
-    })
-
-    // 切断されたくり抜き領域を差し引いたポリゴンを生成
-    const splitedAfterNot = splited.map(s => {
-      return notPolyList.reduce((p, c) => {
-        return okageo.geo.getPolygonNotPolygon(p, c)
-      }, s)
-    })
-
-    // 包含関係で再度グルーピング
-    const groups = okageo.geo.getIncludedPolygonGroups(splitedAfterNot)
-
-    // 分割後shape生成
-    const splitedShapeList: ISvgPath[] = []
-    groups.forEach(group => {
-      const [path, ...included] = group
-      splitedShapeList.push({ d: path, included, style: shape.style })
-    })
-
-    return splitedShapeList
+    const block = craeteBlock(pathInfo, this.faceMaterial, this.wallMaterial)
+    this.scene.add(block.face, block.wall)
+    this.blocks.push(block)
   }
 
   async importFromString(text: string) {
     const pathInfoList = await parseFont(text, this.style)
-    let splited = pathInfoList.concat()
-    ;[...Array(100)].forEach((_, x) => {
-      const line = [
-        { x: -500 + x * 10, y: -500 },
-        { x: -500 + x * 10, y: 500 },
-      ]
-      let list: ISvgPath[] = []
-      splited.forEach(path => {
-        list = list.concat(this.splitShape(path, line))
-      })
-      splited = list
-    })
-
-    const size = 50
+    const spaceSize = 50
     okageo.svg
       .fitRect(
-        splited.map(path => ({ d: path.d, style: this.style })),
-        -size / 2,
-        -size / 2,
-        size,
-        size,
+        splirtGrid(pathInfoList).map(path => ({ d: path.d, style: this.style })),
+        -spaceSize / 2,
+        -spaceSize / 2,
+        spaceSize,
+        spaceSize,
       )
       .forEach(p => this.createMesh(p))
   }
@@ -230,14 +96,17 @@ export default class App {
   }
 
   clear() {
-    this.meshes.forEach(m => this.scene.remove(m))
-    this.meshes = []
+    this.blocks.forEach(b => {
+      this.scene.remove(b.face)
+      this.scene.remove(b.wall)
+    })
+    this.blocks = []
   }
 
   dispose() {
     this.running = false
     this.renderer.dispose()
     this.scene.dispose()
-    this.material.dispose()
+    this.faceMaterial.dispose()
   }
 }
---
+++ src/utils.ts
@@ -0,0 +1,200 @@
+import * as THREE from 'three'
+import { Font, load } from 'opentype.js'
+import okageo, { IVec2, ISvgPath, ISvgStyle } from 'okageo'
+
+export interface IBlock {
+  face: THREE.Mesh
+  wall: THREE.Mesh
+  position: THREE.Vector3
+}
+
+let _font: Font | null
+const fontURL = 'https://fonts.gstatic.com/ea/notosansjapanese/v6/NotoSansJP-Medium.otf'
+
+function loadFont(): Promise<Font> {
+  return new Promise((resolve, reject) => {
+    if (_font) resolve(_font)
+
+    load(fontURL, (err, font) => {
+      if (!font || err) {
+        return reject(new Error('Font could not be loaded: ' + err))
+      }
+      return resolve(font)
+    })
+  })
+}
+
+export async function parseFont(text: string, style: ISvgStyle): Promise<ISvgPath[]> {
+  const font = await loadFont()
+  const lines = text.split(/\n|\r\n/)
+  const size = 72
+  let pathList: IVec2[][] = []
+  lines.forEach((line, i) => {
+    pathList = pathList.concat(
+      okageo.svg
+        .parseOpenPath(font.getPath(line, 0, size * 1.1 * i, size))
+        .map(info => okageo.geo.omitSamePoint(info.d)),
+    )
+  })
+
+  return okageo.geo.getIncludedPolygonGroups(pathList).map(group => {
+    const [d, ...included] = group
+    return { d, included, style }
+  })
+}
+export function splitShape(shape: ISvgPath, line: IVec2[]): ISvgPath[] {
+  let splited = okageo.geo.splitPolyByLine(shape.d, line)
+  if (splited.length < 2) return [shape]
+
+  // 本体と回転方向が一致しているかで分類
+  const rootLoopwise = okageo.geo.getLoopwise(shape.d)
+  const sameLoopwiseList: IVec2[][] = []
+  const oppositeLoopwiseList: IVec2[][] = []
+  if (shape.included) {
+    shape.included.forEach(s => {
+      if (okageo.geo.getLoopwise(s) === rootLoopwise) {
+        sameLoopwiseList.push(s)
+      } else {
+        oppositeLoopwiseList.push(s)
+      }
+    })
+  }
+
+  // 本体と同回転のものはそのまま分割
+  sameLoopwiseList.forEach(poly => {
+    const sp = okageo.geo.splitPolyByLine(poly, line)
+    splited = [...splited, ...(sp.length > 0 ? sp : [poly])]
+  })
+
+  // 本体と逆回転のものは特殊処理
+  const notPolyList: IVec2[][] = []
+  oppositeLoopwiseList.forEach(poly => {
+    const sp = okageo.geo.splitPolyByLine(poly, line)
+    if (sp.length > 0) {
+      // 分割されたらブーリアン差をとるために集める
+      notPolyList.push(poly)
+    } else {
+      // 分割なしならそのまま
+      splited.push(poly)
+    }
+  })
+
+  // 切断されたくり抜き領域を差し引いたポリゴンを生成
+  const splitedAfterNot = splited.map(s =>
+    notPolyList.reduce((p, c) => okageo.geo.getPolygonNotPolygon(p, c), s),
+  )
+
+  return okageo.geo.getIncludedPolygonGroups(splitedAfterNot).map(group => {
+    const [path, ...included] = group
+    return { d: path, included, style: shape.style }
+  })
+}
+
+export function getMaxSize(
+  pathInfoList: ISvgPath[],
+): { x: number; y: number; width: number; height: number } {
+  let minX = Infinity
+  let minY = Infinity
+  let maxX = -Infinity
+  let maxY = -Infinity
+
+  pathInfoList.forEach(path =>
+    path.d.forEach(v => {
+      minX = Math.min(minX, v.x)
+      minY = Math.min(minY, v.y)
+      maxX = Math.max(maxX, v.x)
+      maxY = Math.max(maxY, v.y)
+    }),
+  )
+
+  return {
+    x: minX,
+    y: minY,
+    width: maxX - minX,
+    height: maxY - minY,
+  }
+}
+
+export function splirtGrid(pathInfoList: ISvgPath[]): ISvgPath[] {
+  const size = getMaxSize(pathInfoList)
+  let splited = pathInfoList.concat()
+  ;[...Array(Math.ceil(size.width / 10))].forEach((_, i) => {
+    const line = [
+      { x: size.x + i * 10, y: size.y },
+      { x: size.x + i * 10, y: size.y + size.height },
+    ]
+    let list: ISvgPath[] = []
+    splited.forEach(path => {
+      list = list.concat(splitShape(path, line))
+    })
+    splited = list
+  })
+  ;[...Array(Math.ceil(size.height / 10))].forEach((_, i) => {
+    const line = [
+      { x: size.x, y: size.y + i * 10 },
+      { x: size.x + size.width, y: size.y + i * 10 },
+    ]
+    let list: ISvgPath[] = []
+    splited.forEach(path => {
+      list = list.concat(splitShape(path, line))
+    })
+    splited = list
+  })
+
+  return splited
+}
+
+export function craeteBlock(
+  pathInfo: ISvgPath,
+  faceMaterial: THREE.Material,
+  wallMaterial: THREE.Material,
+): IBlock {
+  const depth = 2
+  const size2D = getMaxSize([pathInfo])
+  const center = new THREE.Vector3(
+    size2D.x + size2D.width / 2,
+    -(size2D.y + size2D.height / 2),
+    depth / 2,
+  )
+  const wallGeometry = new THREE.Geometry()
+  const shape = new THREE.Shape()
+
+  pathInfo.d.forEach((v, i) => {
+    const from = { x: v.x, y: -v.y }
+    const to = {
+      x: pathInfo.d[(i + 1) % pathInfo.d.length].x,
+      y: -pathInfo.d[(i + 1) % pathInfo.d.length].y,
+    }
+
+    if (i === 0) shape.moveTo(from.x, from.y)
+    else shape.lineTo(from.x, from.y)
+
+    // はみ出ないよう側面はdepthより短くする
+    const mesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(okageo.geo.getDistance(from, to), depth * 0.95),
+    )
+    const fromCenter = okageo.geo.getCenter(from, to)
+    mesh.translateX(fromCenter.x)
+    mesh.translateY(fromCenter.y)
+    mesh.translateZ(-depth / 2)
+    mesh.rotateX(-Math.PI / 2)
+    mesh.rotateY(-okageo.geo.getRadian(to, from))
+    wallGeometry.mergeMesh(mesh)
+  })
+
+  wallGeometry.center()
+  const wallMesh = new THREE.Mesh(wallGeometry, wallMaterial)
+  wallMesh.position.set(center.x, center.y, center.z)
+
+  const shapeMeshTop = new THREE.Mesh(new THREE.ShapeGeometry(shape))
+  const shapeMeshBottom = shapeMeshTop.clone()
+  shapeMeshBottom.translateZ(-depth)
+  const shapeGeometry = new THREE.Geometry()
+  shapeGeometry.mergeMesh(shapeMeshTop)
+  shapeGeometry.mergeMesh(shapeMeshBottom)
+  shapeGeometry.center()
+  const shapeMesh = new THREE.Mesh(shapeGeometry, faceMaterial)
+  shapeMesh.position.set(center.x, center.y, center.z)
+
+  return { face: shapeMesh, wall: wallMesh, position: center }
+}
